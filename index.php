046107dd075a561d08787be4aff7c6f89645a475
RivaldoNeves <rivaldoantonioneves@gmail.com>
2025-04-02 16:25:49 +0200

    Atualização

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
     echo'<h1>Hello, World</h1>';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-Vndo a NeveStar!</title>
+</head>
+<body>
+    
+</body>
+</html>
